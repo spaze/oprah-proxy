ce23b01ae0bca0545c82d10cf0b407df525cdf90
Michal Špaček <mail@michalspacek.cz>
2016-04-24 21:32:52 +0200

    Adding proxy creds generator

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# oprah-proxy
-Generate credentials for Opera's "browser VPN"
+# The Oprah Proxy
+*Generate credentials for Opera's "browser VPN"*
+
+!["Everybody gets a proxy" logo](logo.jpg)
+
+Opera "VPN" introduced in Opera 38 Developer Edition is not a VPN, it's actually a proxy for the browser only, but it's free and unlimited so why not use it for some other apps, too.
+
+## Usage
+Just run `oprah-proxy.py`. It will generate credentials for you and list available proxies so you can use them elsewhere. Unfortunately I've had no success using any of these proxies as a proxy for `curl` for example. But it works with OpenSSL's `s_client` tool and the script will also print the required command for you.
+
+## Requirements
+Python 3 & [Requests](http://docs.python-requests.org/). The API server works only with clients with (Server Name Indication) SNI support. Python 3 and Python 2.7.9+ include native support for SNI in their TLS modules. I was too lazy to build a version check, so I went with Python 3.
+
+## Technical details
+The proxy is a *secure* one, which means the browser talks to the proxy server via HTTPS even if it loads a plain HTTP site. Hostname resolution (*DNS*) is also done remotely on the proxy server, so the browser does not leak hostnames when using this proxy. Currently, Opera leaks IP address via WebRTC and plugins, but Opera Software is aware of it and plans to fix it in a future release.
+
+For more technical details, including HTTP API calls, see my [technical write-up](https://gist.github.com/spaze/558b7c4cd81afa7c857381254ae7bd10).
+
+## A message to Opera
+Opera insists to call this *a browser VPN*. Sorry Opera, it's not a VPN, and calling it *a browser VPN* does not make it one. A VPN protects all connections and not just the browser ones. We've been teaching people that VPN protects their devices and operating systems and then you bake a proxy, although a secure one, in the browser and call it a VPN, that's bullshit, *pardon my French*. The feature is quite nice, but calling it *a VPN* could eventually be harmful to privacy, because of false sense of security.
+
+## Disclaimer
+This is just for research purposes, to study how things work, no harm intended.
+
+## Example `openssl s_client` command:
+```
+URL="http://www.opera.com" PROXY=159.203.42.222:443 HEADER="Proxy-Authorization: MURBNTY1NDRFMkQ4NUZEMTgxRDY2OUUxNzM1ODg1MjI3QTRFQUNGQzpGQTI3NzIyMzhEMzg2MzlDMzYzQjk0RTA2MDc3NUIzNzMyNkIyQUEzQTM3OEVBNTdCOEVGQTUxQ0EzMjg0Qjc5"; echo -e "GET $URL HTTP/1.0\n$HEADER\n\n" | openssl s_client -connect $PROXY -ign_eof
+```
+Where
+- `URL` is a URL you want to load, change it to fit your needs
+- `PROXY` is a proxy from the list (the one in the example is the first listed proxy with port 443), change it if you want
+- `HEADER` is a `Proxy-Authorization` HTTP header using generated credentials, no need to change this
---
+++ oprah-proxy.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+
+import requests, uuid, hashlib, urllib.parse, base64
+
+class OprahProxy:
+	''' Everybody gets a proxy! '''
+
+	client_type = ''
+
+	client_key = ''
+
+	session = None
+
+	device_id = ''
+
+	device_id_hash = ''
+
+	device_password = ''
+
+	example_proxy = None
+
+
+	def __init__(self, client_type, client_key):
+		self.client_type = client_type
+		self.client_key = client_key
+		self.session = requests.Session()
+
+
+	def post(self, url, data):
+		headers = {
+			'SE-Client-Type': self.client_type,
+			'SE-Client-API-Key': self.client_key,
+		}
+		result = self.session.post('https://api.surfeasy.com%s' % url, data, headers = headers).json()
+		code = list(result['return_code'].keys())[0]
+		if (code != '0'):
+			print('ERROR: %s' % result['return_code'][code])
+			exit(1);
+		return result
+
+
+	def register_subscriber(self):
+		print('DEBUG: Call register_subscriber')
+		email_user = uuid.uuid4()
+		email = '%s@mailinator.com' % email_user
+		password = uuid.uuid4()
+		password_hash = hashlib.sha1(str(password).encode('ascii')).hexdigest().upper()
+		print('DEBUG: Your SurfEasy email: %s' % email)
+		print('DEBUG: Your SurfEasy password: %s' % password)
+		print('DEBUG: Your SurfEasy password hash: %s' % password_hash)
+		print('DEBUG: Your mailbox: https://mailinator.com/inbox2.jsp?%s' % urllib.parse.urlencode({'public_to': email_user}))
+		print('DEBUG: These are not the credentials you are looking for (you won\'t probably need these, ever)')
+
+		data = {
+			'email': email,
+			'password': password_hash
+		}
+		result = self.post('/v2/register_subscriber', data)
+		print('DEBUG: Subscriber registered')
+
+
+	def you_get_a_proxy(self):
+		print('++++++++++++++++++++++++++=======================~~~~~~~::::')
+		print(',..,,,.,,...,,,.,,,,,,,,,,............,.....,.,,::::~~======')
+		print(',,,,,:,,,,,,,,,,,,,,                    ,.,,,,,,,,,,,,,....~')
+		print('~~~~~~~:::::::::::::  YOU GET A PROXY!  ,,,,,,,,,,,,,,,,,:~=')
+		print('~~===~~~~::::::::::.                    ,,,,,,,,,,,,,,,~~~:~')
+		print('::~:::::::::::::::,,...,~=::~=~:.....,,,,,,,,,,,,,,,:~:~:,::')
+		print('??+=..,++++++++++=.....:===I+~=~....,,~~=~~~~~~~:~====~,,::~')
+		print('::~....,...............~=~~~~:=~,...,.~~~======~==++=~:====+')
+		print('::~...,:,,,,,,,,.......:==~+=:~:,.....,,,,,,,~~~====~,,,,,,,')
+		print(':::~~~:,::,,::::,......+~~===~:,.....,,,,,,~~~~~==~,,,,....,')
+		print('~:~:,,,,,:::::,..........~~::~,:....:~~~~~::::~:::::,,,,,,,,')
+		print('~...~~~,,::~~::~:........~~~:~::.:,,:~::::::::::::::::::::::')
+		print('+..???+?~,::::::::::::~:~::~~~:~::::::::::::::::~~=~~~~~~~~=')
+		print('...?????+?,.:::::::::::~~:~~::~:::::::::::::::,~++++++++++++')
+		print('?=??????+??~,::::::::~~~~~~~~~~::::::::::::::,=+++++++++++++')
+		print('????????????::::::,::~~~~~~~~~~:::::::::::::,+++++++++++++++')
+		print('??+??????????:,,::,:::~~~~~~~:::::::::::::,+++++++++++++++++')
+		print('??????????????+,,,,:::~~,~~~~~:::=,:::::,?+?++++++++++++++++')
+		print('????????????????+,:::~~:,~~~~~~~~:~::::=+??+?++?++++++++++++')
+		print('??????????????????:::~~~~~~~~~~~:::::::???????++++++++++++++')
+		print('??????????????????=:::~~~~~~~~~::::::::+??????+++++?+?++++++')
+		print('=+==+====++++++++==:::~:,~~~~~:::,::::~====~~::::,,...,:~~=+')
+		print('++++++++++++===++++::~:~~~~~~::::~::::=+++++++++++++========')
+		print('=====+++++=====++++:~:~~~~~~::::::::::~++===================')
+		print('+++==+======+++                           ==================')
+		print('~~~~==========~  EVERYBODY GETS A PROXY!  ~~~~~~~~~~~~~~~~~~')
+		print('~~~~~~~~~~~===~                           ~~~~~~~~~~~~~~~~~~')
+		print('=============++===:::::::::::::::::::::~~~~~~~~~~~~~~~~~~~:~')
+		print('https://github.com/spaze/oprah-proxy :::==~=~~~~~~~~=~~~~~~~')
+
+
+	def register_device(self):
+		print('DEBUG: Call register_device')
+		data = {
+			'client_type': self.client_type,
+			'device_hash': '4BE7D6F1BD040DE45A371FD831167BC108554111',
+			'device_name': 'Opera-Browser-Client'
+		}
+		result = self.post('/v2/register_device', data)
+		self.device_id = result['data']['device_id']
+		self.device_id_hash = hashlib.sha1(str(self.device_id).encode('ascii')).hexdigest().upper()
+		self.device_password = result['data']['device_password']
+		print('DEBUG: Device registered')
+
+
+	def geo_list(self):
+		print('DEBUG: Call geo_list')
+		data = {
+			'device_id': self.device_id_hash
+		}
+		result = self.post('/v2/geo_list', data)
+		codes = []
+		for geo in result['data']['geos']:
+			codes.append(geo['country_code'])
+			print('INFO: Supported country: %s %s' % (geo['country_code'], geo['country']))
+		print('DEBUG: Geo list fetched')
+		return codes
+
+
+	def discover(self, country_code):
+		print('DEBUG: Call discover %s' % country_code)
+		data = {
+			'serial_no': self.device_id_hash,
+			'requested_geo': '"%s"' % country_code
+		}
+		result = self.post('/v2/discover', data)
+
+		for ip in result['data']['ips']:
+			for port in ip['ports']:
+				if port == 443 and self.example_proxy is None:
+					self.example_proxy = '%s:%s' % (ip['ip'], port)
+				print('INFO: Proxy in %s %s:%s' % (ip['geo']['country_code'], ip['ip'], port))
+		print('DEBUG: Proxies discovered')
+
+
+	def everybody_gets_a_proxy(self):
+		self.register_subscriber()
+		self.register_device()
+		for country_code in self.geo_list():
+			self.discover(country_code)
+		print('INFO: Pick a proxy from the list above and use these credentials:')
+		print('INFO: Username: %s' % self.device_id_hash)
+		print('INFO: Password: %s' % self.device_password)
+		creds = ('%s:%s' % (self.device_id_hash, self.device_password)).encode('ascii')
+		header = 'Proxy-Authorization: %s' % base64.b64encode(creds).decode('ascii')
+		print('INFO: HTTP header %s' % header)
+		print('DEBUG: Example bash command: URL="http://www.opera.com" PROXY=%s HEADER="%s"; echo -e "GET $URL HTTP/1.0\\n$HEADER\\n\\n" | openssl s_client -connect $PROXY -ign_eof' % (self.example_proxy, header))
+		print('DEBUG: GET ... HTTP/1.1 does not work with these proxies, only GET ... HTTP/1.0')
+		print('DEBUG: Sorry, I couldn\'t get curl to send a request via such HTTPS proxy, so it\'s s_client for now')
+
+
+you_get_a_proxy = OprahProxy('se0304', '3690AC1CE5B39E6DC67D9C2B46D3C79923C43F05527D4FFADCC860740E9E2B25')
+you_get_a_proxy.you_get_a_proxy()
+you_get_a_proxy.everybody_gets_a_proxy()
